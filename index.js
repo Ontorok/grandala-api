42627aa7631dcb641a326ed12dbb5f17d24e97ec
Ontorok <nasir.amath07@gmail.com>
2021-11-14 15:55:19 +0600

    seeding complete

--- index.js
+++ index.js
@@ -10,23 +10,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 /* -------------------- External Imports (end) -------------------- */
 
 /* -------------------- Internal Imports (start) -------------------- */
 const { userRouter } = require('./routes');
+const People = require('./models/People');
 /* -------------------- Internal Imports (end) -------------------- */
 
 dotenv.config();
 const app = express();
+const seedDB = async () => {
+  console.log('Seeding start');
+  await People.deleteMany({});
+  await People.insertMany([
+    {
+      username: 'nasir',
+      email: 'nasir@mail.com',
+      password: 'Nasir@123',
+      isAdmin: true
+    },
+    {
+      username: 'ahmed',
+      email: 'ahmed@mail.com',
+      password: 'Aasir@123',
+      isAdmin: false
+    }
+  ]);
+  console.log('Seeding completed');
+};
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log('DB Connection established!!');
   })
+  .then(() => seedDB().then(() => mongoose.connection.close()))
   .catch((err) => {
     console.log(err.message);
   });
+
 app.use(express.json());
 app.use('/api/user', userRouter);
 
